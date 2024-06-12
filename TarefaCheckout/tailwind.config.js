71e34fabe16ad759889a6b3be866deefdaf3aff1
João Paulo Lima Ferreira <joaopaulolfdev@gmail.com>
2024-06-12 14:34:38 -0300

    feat: start checkout project

---
+++ TarefaCheckout/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/views/welcome.blade.php",
+    "./resources/components/app.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
